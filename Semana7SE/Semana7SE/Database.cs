d2aeab89d5ede1f03f5057ea5d8dc58b46482cc6
santiago gabriel escobar murillo <santy1025@hotmail.com>
2020-11-21 20:24:45 -0500

    Add project files.

---
+++ Semana7SE/Semana7SE/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Semana7SE
+{
+    public interface Database
+    {
+        //variable de conexion
+        SQLiteAsyncConnection GetConnection();
+    }
+}
